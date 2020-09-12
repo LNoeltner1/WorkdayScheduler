35e31c5a6697e1987b70468107b09aa922b844ac
Lauren Noeltner <laurennoeltner@gmail.com>
2020-09-12 01:01:26 -0400

    set up basic structure of page

---
+++ script.js
@@ -0,0 +1,18 @@
+var hoursList = [
+  $("#nineAM"),
+  $("#tenAM"),
+  $("#elevenAM"),
+  $("#twelvePM"),
+  $("#onePM"),
+  $("#twoPM"),
+  $("#threePM"),
+  $("#fourPM"),
+  $("#fivePM"),
+];
+console.log(hoursList[2]);
+
+if (i < 4) {
+  hoursList[i].style.background = "green";
+}
+// } else if (i > currentHour) {
+//   hoursList[].style.background = green;
